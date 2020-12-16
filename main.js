a72cf1c04062d097743cd8e20501409684cbf3e6
josep rigo <jrrr13@gmail.com>
2020-12-16 10:42:08 +0100

    08 components with components

--- main.js
+++ main.js
@@ -1,9 +1,25 @@
+Vue.component('task-list',{
+	template: '<div><task v-for="task in tasks" >{{ task.name }}</task></div>',
+
+	data(){
+		return {
+			tasks: [
+				{name: 'Do the laundry', completed: false},
+				{name: 'Cook lunch', completed: true},
+				{name: 'Go to school', completed: false},
+				{name: 'Do english homework', completed: false},
+				{name: 'Call grandma', completed: true},
+				]
+		}
+	}
+});
+
 Vue.component('task',{
 	template: '<li><slot></slot></li>',
 
 	data(){
 		return {
-			message 'Important data in a component is a function that returns an object'
+			message: 'task'
 		}
 	}
 });
