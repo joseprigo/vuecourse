898f9a937a6c6af02c925fb6ff5e466a9a768b65
josep rigo <jrrr13@gmail.com>
2020-12-16 15:10:58 +0100

    15 inline templates

--- main.js
+++ main.js
@@ -1,28 +1,10 @@
-Vue.component('modal',{
+Vue.component('progress-view',{
 
-	template: `
-	<div class="modal is-active">
-	  <div class="modal-background"></div>
-	  <div class="modal-card">
-	    <header class="modal-card-head">
-	      <p class="modal-card-title">
-	      	<slot name="header"></slot>
-	      </p>
-	      <button class="delete" aria-label="close"></button>
-	    </header>
-	    <section class="modal-card-body">
-	      <slot name="content">
-	      </slot>
-	    </section>
-	    <footer class="modal-card-foot">
-	    <slot name="footer">
-	    <button class="button">Okay</button>
-	    </slot>
-	      
-	    </footer>
-	  </div>
-	</div>
-	`
+	data(){
+		return{
+			completionRate: 0
+		}
+	}
 
 });
 
