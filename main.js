ae541034adc7c69ee5cdec4025f6bf83675d1a1e
josep rigo <jrrr13@gmail.com>
2020-12-16 11:46:04 +0100

    10 partial components 2 modal

--- main.js
+++ main.js
@@ -1,15 +1,29 @@
+Vue.component('modal-message',{
+	template:`
+	<div class="modal is-active">
+ 		<div class="modal-background"></div>
+  		<div class="modal-content">
+  			<div class="box">
+				<slot></slot>
+			</div>
+  		</div>
+  		<button class="modal-close is-large" aria-label="close" @click="$emit('close')"></button>
+	</div>
+	`
+});
+
 Vue.component('message-raw',{
 	props: ['title', 'body'],
 	template: `
 	<article class="message" v-show="isVisible">
-  <div class="message-header">
-    <p>{{ title }}</p>
-    <button class="delete" aria-label="delete" @click="isVisible=false" ></button>
-  </div>
-  <div class="message-body">
-    {{ body}}
-  </div>
-</article>
+  		<div class="message-header">
+    		<p>{{ title }}</p>
+    		<button class="delete" aria-label="delete" @click="isVisible=false" ></button>
+  		</div>
+  		<div class="message-body">
+    		{{ body}}
+  		</div>
+	</article>
 `,
 	data(){
 		return {
@@ -19,5 +33,8 @@ Vue.component('message-raw',{
 });
 
 new Vue({
-	el:'#root'
+	el:'#root',
+	data: {
+		showModal: false
+	}
 });
