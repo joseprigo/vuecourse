089d553bd25a60b057c3df254eb92e717450c067
josep rigo <jrrr13@gmail.com>
2020-12-16 14:55:21 +0100

    14 name slots in a nutshell

--- main.js
+++ main.js
@@ -1,44 +1,31 @@
-window.Event = new class {
-
-	constructor(){
-		this.vue = new Vue();
-	}
-
-	fire(event, data = null){
-		this.vue.$emit(event,data);
-	}
-
-	listen(event, callback){
-		this.vue.$on(event,callback)
-
-	}
-};
-
-Vue.component('coupon',{
+Vue.component('modal',{
 
 	template: `
-	<input type="text" placeholder="Enter your coupon" @blur="onCouponApplied">
-	`,
-	methods:{
-		onCouponApplied(){
-			Event.fire('applied', this.coupon);
-
-		}
-	}
+	<div class="modal is-active">
+	  <div class="modal-background"></div>
+	  <div class="modal-card">
+	    <header class="modal-card-head">
+	      <p class="modal-card-title">
+	      	<slot name="header"></slot>
+	      </p>
+	      <button class="delete" aria-label="close"></button>
+	    </header>
+	    <section class="modal-card-body">
+	      <slot name="content">
+	      </slot>
+	    </section>
+	    <footer class="modal-card-foot">
+	    <slot name="footer">
+	    <button class="button">Okay</button>
+	    </slot>
+	      
+	    </footer>
+	  </div>
+	</div>
+	`
 
 });
 
 new Vue({
 	el: '#root',
-	data:{
-		couponApplied:false
-	},
-	methods:{
-		onCouponApplied(){
-			this.couponApplied = true;
-		}
-	},
-	created(){
-		Event.listen('applied', ()=> alert('Handeling it!'));
-	}
 });
